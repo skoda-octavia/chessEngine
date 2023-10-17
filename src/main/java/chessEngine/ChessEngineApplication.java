350f10bfb88148db44854649f154c7a84677a447
Piotr pc <Piotr.kow.59@gmail.com>
2023-10-17 20:20:23 +0200

    project init

---
+++ src/main/java/chessEngine/ChessEngineApplication.java
@@ -0,0 +1,13 @@
+package chessEngine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/chessEngine/ChessEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package chessEngine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
