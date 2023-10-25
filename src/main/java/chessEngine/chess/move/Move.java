f7ada8fedd527462e080e94ad8e603782dbf0661
Piotr pc <Piotr.kow.59@gmail.com>
2023-10-25 15:27:08 +0200

    tests unveiling added, move.equals()

--- src/main/java/chessEngine/chess/move/Move.java
+++ src/main/java/chessEngine/chess/move/Move.java
@@ -6,6 +6,8 @@ import chessEngine.chess.piece.Piece;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Objects;
+
 @Getter
 @Setter
 public class Move {
@@ -13,6 +15,18 @@ public class Move {
     private final Field to;
     private EnginePosition parentPosition;
     private String transformationPieceCode;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Move move)) return false;
+        return from.equals(move.from) && to.equals(move.to);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(from, to, parentPosition, transformationPieceCode);
+    }
 
     public Move(Field from, Field to) {
         this.from = from;
--- src/test/java/chessEngine/chessTest/engine/EngineUnveilingMoveTest.java
+++ src/test/java/chessEngine/chessTest/engine/EngineUnveilingMoveTest.java
@@ -3,7 +3,13 @@ package chessEngine.chessTest.engine;
 import chessEngine.chess.EnginePosition;
 import chessEngine.chess.move.Move;
 import chessEngine.chess.move.field.Field;
+import chessEngine.chess.piece.Piece;
+import chessEngine.chess.piece.constantMovesPiece.knight.BlackKnight;
+import chessEngine.chess.piece.infiniteRangePiece.bishop.WhiteBishop;
+import chessEngine.chess.piece.infiniteRangePiece.queen.WhiteQueen;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -35,5 +41,82 @@ public class EngineUnveilingMoveTest {
         assertEquals(pawnMovesUnveiling, true);
         assertEquals(pawnCaptureUnveiling, false);
 
+    }
+
+    @Test
+    void unveilingMoveTestBishop() {
+        String posCode = "";
+        posCode += "bRbkbBbQ  bBbkbR";
+        posCode += "bPbPbPbPbPbPbPbP";
+        posCode += "    bB  bR      ";
+        posCode += "  wP    wB      ";
+        posCode += "        wK  bKwR";
+        posCode += "                ";
+        posCode += "wPwPwPwP  wPwP  ";
+        posCode += "wRwkwBwQ  wBwk  ";
+        EnginePosition enginePosition = new EnginePosition(posCode, true);
+        enginePosition.set();
+        Piece[][] chessBoard = enginePosition.getChessBoard();
+        WhiteBishop whiteBishop = (WhiteBishop) chessBoard[3][4];
+        ArrayList<Move> bishopMoves = whiteBishop.possibleMoves(enginePosition.getColorMap());
+        for (Move move : bishopMoves) {
+            assertEquals(enginePosition.unveilingMove(move), true);
+        }
+    }
+
+    @Test
+    void unveilingMoveTestQueen() {
+        String posCode = "";
+        posCode += "bRbkbBbQbKbBbkbR";
+        posCode += "bPbPbPbPbPbPbPbP";
+        posCode += "    bB  bR      ";
+        posCode += "  wP  wPwQ      ";
+        posCode += "        wK    wR";
+        posCode += "                ";
+        posCode += "wPwPwPwP  wPwP  ";
+        posCode += "wRwkwBwQ  wBwk  ";
+        EnginePosition enginePosition = new EnginePosition(posCode, true);
+        enginePosition.set();
+        Piece[][] chessBoard = enginePosition.getChessBoard();
+        WhiteQueen whiteQueen = (WhiteQueen) chessBoard[3][4];
+        ArrayList<Move> queenMoves = whiteQueen.possibleMoves(enginePosition.getColorMap());
+        int correctMovesCount = 0;
+        Move correctMove = null;
+        for (Move move : queenMoves) {
+            boolean correct = !enginePosition.unveilingMove(move);
+            if (correct) {
+                correctMovesCount += 1;
+                correctMove = move;
+            }
+        }
+        assertEquals(correctMovesCount, 1);
+        assertEquals(
+                correctMove,
+                new Move(
+                        new Field((byte)3, (byte)4),
+                        new Field((byte)2, (byte)4)
+                )
+        );
+    }
+
+    @Test
+    void unveilingMoveTestBlackKnight() {
+        String posCode = "";
+        posCode += "bRbkbBbQbKbBbkbR";
+        posCode += "bPbPbPbPbkbPbPbP";
+        posCode += "    bB          ";
+        posCode += "  wP  wPwQ      ";
+        posCode += "              wR";
+        posCode += "                ";
+        posCode += "wPwPwPwP  wPwP  ";
+        posCode += "wRwkwBwQwKwBwk  ";
+        EnginePosition enginePosition = new EnginePosition(posCode, false);
+        enginePosition.set();
+        Piece[][] chessBoard = enginePosition.getChessBoard();
+        BlackKnight blackKnight = (BlackKnight) chessBoard[1][4];
+        ArrayList<Move> knightsMoves = blackKnight.possibleMoves(enginePosition.getColorMap());
+        for (Move move : knightsMoves) {
+            assertEquals(enginePosition.unveilingMove(move), true);
+        }
     }
 }
